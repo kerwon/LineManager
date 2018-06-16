54453d49208f4b68a9fefa40ac8b4bef406768fb
Kerwon <kerwin1000@163.com>
2018-06-16 16:00:04 +0800

    dev init

---
+++ LineManager-ok/LineManager/ExcelHelper.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Npoi.Mapper;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using NPOI.XSSF.UserModel;
+
+namespace LineManager
+{
+    public partial class ExcelHelper
+    {
+
+        public static async Task<List<KuCunInfoView>> ReadKuCunInfosExcel(string filePath)
+        {
+            var result = await Task.Run(() =>
+            {
+                List<KuCunInfoView> viewData = null;
+                using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+
+                    var mapper = new Mapper(fs);
+
+                    var data = mapper.Take<KuCunInfoView>("库存信息");
+                    if (data != null && data.Any())
+                    {
+                        viewData = data.Select(m => m.Value).ToList();
+                    }
+                }
+
+                return viewData;
+            });
+
+            return result ?? throw new Exception("获取库存信息出错!");
+        }
+
+
+        public static List<KuCunInfoView> ReadKuCunInfosExcel(MemoryStream ms)
+        {
+            var mapper = new Mapper(ms);
+
+            var data = mapper.Take<KuCunInfoView>("库存信息");
+            if (data != null && data.Any())
+            {
+                return data.Select(m => m.Value).ToList();
+            }
+
+            throw new Exception("获取库存信息出错!");
+        }
+
+
+        public static async Task<List<FaHuoInfo>> ReadFaHuoInfosExcel(string filePath)
+        {
+
+            var result = await Task.Run(() =>
+            {
+                List<FaHuoInfo> viewData = null;
+                using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    var mapper = new Mapper(fs);
+
+                    mapper.Map<FaHuoInfo>("序号", o => o.CustomerId)
+                        .Map(column => true,
+                            (column, target) =>
+                            {
+                                if (column.CurrentValue == null)
+                                    return true;
+
+                                var inx = column.Attribute.Index / 2;
+                                var mod = column.Attribute.Index % 2;
+                                var isNew = mod == 1;
+
+                                var faHuo = target as FaHuoInfo;
+                                if (faHuo == null)
+                                    throw new Exception("读取发货任务信息Excel数据出错!");
+
+                                FaHuoItem item = isNew ? new FaHuoItem() : faHuo.Items.Last();
+
+
+                                var tmpCellVal = column.CurrentValue?.ToString() ?? string.Empty;
+
+                                if (column.HeaderValue.Equals("型号"))
+                                {
+                                    item.XingHao = tmpCellVal;
+                                }
+
+                                if (column.HeaderValue.Equals("长度"))
+                                {
+                                    int.TryParse(tmpCellVal, out int tmpval);
+                                    item.Length = tmpval;
+                                }
+
+                                if (isNew)
+                                    faHuo.Items.Add(item);
+                                return true;
+                            });
+
+
+                    var data = mapper.Take<FaHuoInfo>("发货任务信息");
+
+                    if (data != null && data.Any())
+                    {
+                        viewData = data.Select(m => m.Value).ToList();
+                    }
+                }
+                return viewData;
+            });
+
+            return result ?? throw new Exception("获取发货任务信息出错!");
+        }
+
+
+        public static List<FaHuoInfo> ReadFaHuoInfosExcel(MemoryStream ms)
+        {
+            var mapper = new Mapper(ms);
+
+            mapper.Map<FaHuoInfo>("序号", o => o.CustomerId)
+                .Map(column => true,
+                    (column, target) =>
+                    {
+                        if (column.CurrentValue == null)
+                            return true;
+
+                        var inx = column.Attribute.Index / 2;
+                        var mod = column.Attribute.Index % 2;
+                        var isNew = mod == 1;
+
+                        var faHuo = target as FaHuoInfo;
+                        if (faHuo == null)
+                            throw new Exception("读取发货任务信息Excel数据出错!");
+
+                        FaHuoItem item = isNew ? new FaHuoItem() : faHuo.Items.Last();
+
+
+                        var tmpCellVal = column.CurrentValue?.ToString() ?? string.Empty;
+
+                        if (column.HeaderValue.Equals("型号"))
+                        {
+                            item.XingHao = tmpCellVal;
+                        }
+
+                        if (column.HeaderValue.Equals("长度"))
+                        {
+                            int.TryParse(tmpCellVal, out int tmpval);
+                            item.Length = tmpval;
+                        }
+
+                        if (isNew)
+                            faHuo.Items.Add(item);
+                        return true;
+                    });
+
+
+            var data = mapper.Take<FaHuoInfo>("发货任务信息");
+
+            if (data != null && data.Any())
+            {
+                return data.Select(m => m.Value).ToList();
+            }
+
+            throw new Exception("获取发货任务信息出错!");
+        }
+
+
+
+        public static void DownLoad(List<LineResultInfo> resultInfos, string filePath)
+        {
+            //var mapperFirst = new Mapper();
+            //var data = LineResultInfoView.MapLineResultInfoView(resultInfos);
+
+            //var result = data.OrderBy(m => m.出库目的地).ThenBy(m => m.电缆型号).ToList();
+            //mapperFirst.Put(data, "线缆出库信息表", true);
+            //mapperFirst.Save(filePath);
+
+            //return;
+
+            IWorkbook wkbook = new XSSFWorkbook();
+            //2. 在该工作簿中创建工作表  
+            ISheet sheet = wkbook.CreateSheet("线缆出库信息表");
+
+            // 向该工作表中插入行和单元格  
+            var rowIndex = 0;
+            var colIndex = 0;
+
+            IRow firstRow = sheet.CreateRow(rowIndex++);
+            firstRow.CreateCell(0).SetCellValue("线缆出库信息一览表");
+            sheet.AddMergedRegion(new CellRangeAddress(0, 0, 0, 6));
+
+            IRow headRow = sheet.CreateRow(rowIndex++);
+            // 在该行中创建单元格  
+            headRow.CreateCell(colIndex++).SetCellValue("出库目的地");
+            headRow.CreateCell(colIndex++).SetCellValue("电缆型号");
+            headRow.CreateCell(colIndex++).SetCellValue("生产厂家");
+            headRow.CreateCell(colIndex++).SetCellValue("线轮位置");
+            headRow.CreateCell(colIndex++).SetCellValue("是否整轮");
+            headRow.CreateCell(colIndex++).SetCellValue("出库长度");
+            headRow.CreateCell(colIndex++).SetCellValue("剩余长度");
+
+            //var lookUpLines = resultInfos.Where(m => m.CustomerId == "1").ToLookup(m => new { m.CustomerId });
+            var lookUpLines = resultInfos.ToLookup(m => new { m.CustomerId });
+
+            foreach (var lookUpCustomerLine in lookUpLines)
+            {//出库目的地
+                colIndex = 0;
+                IRow row = sheet.CreateRow(rowIndex);
+                var customerCell = row.CreateCell(colIndex++);
+                customerCell.SetCellValue(lookUpCustomerLine.Key.CustomerId);
+
+                var firstRowIndex = rowIndex;
+                var totalRowCount = 0;
+                var lookupXingHao = lookUpCustomerLine.ToLookup(m => new { m.XingHao });
+
+                var xinghaoFirst = true;
+                foreach (var xinghaoGroup in lookupXingHao)
+                {
+                    var tmpCount = xinghaoGroup.Count();
+                    totalRowCount += tmpCount;
+
+                    if (xinghaoFirst)
+                    {
+                        xinghaoFirst = false;
+
+                        var xingHaoCell = row.CreateCell(1);
+                        xingHaoCell.SetCellValue(xinghaoGroup.Key.XingHao);
+                        sheet.AddMergedRegion(new CellRangeAddress(rowIndex, firstRowIndex + totalRowCount - 1, 1, 1));
+                    }
+                    else
+                    {
+                        row = sheet.CreateRow(++rowIndex);
+                        var xingHaoCell = row.CreateCell(1);
+                        xingHaoCell.SetCellValue(xinghaoGroup.Key.XingHao);
+                        sheet.AddMergedRegion(new CellRangeAddress(rowIndex, firstRowIndex + totalRowCount - 1, 1, 1));
+                    }
+
+                    var brandFirst = true;
+                    //var newRowIndex = firstRowIndex + 1;
+                    var tmpInx = 0;
+                    var tmpList = xinghaoGroup.OrderBy(p => p.PaiHao).ThenBy(m => m.LunHao).ToList();
+                    foreach (var lineResultInfo in tmpList)
+                    {
+                        tmpInx++;
+                        colIndex = 2;
+
+                        var paiLunHaoFlag = lineResultInfo.PaiHao.GetValueOrDefault() == 0
+                                            || lineResultInfo.LunHao.GetValueOrDefault() == 0;
+
+                        var paiLunHaoCell = paiLunHaoFlag
+                            ? "库存不足"
+                            : $"{lineResultInfo.PaiHao}排 {lineResultInfo.LunHao}轮";
+
+                        var isWheel = lineResultInfo.LeaveLength.HasValue && lineResultInfo.LeaveLength == 0
+                            ? "是"
+                            : lineResultInfo.IsWheel;
+
+                        if (brandFirst)
+                        {
+                            row.CreateCell(colIndex++).SetCellValue(lineResultInfo.Brand);
+                            row.CreateCell(colIndex++).SetCellValue(paiLunHaoCell);
+                            row.CreateCell(colIndex++).SetCellValue(isWheel);//lineResultInfo.IsWheel);
+                            row.CreateCell(colIndex++).SetCellValue(lineResultInfo.Length.GetValueOrDefault());
+                            row.CreateCell(colIndex++).SetCellValue(lineResultInfo.LeaveLength.GetValueOrDefault());
+                            brandFirst = false;
+                        }
+                        else
+                        {
+                            //if (tmpInx != tmpCount)
+                            //    rowIndex++;
+                            var newRow = sheet.CreateRow(++rowIndex);
+
+                            newRow.CreateCell(colIndex++).SetCellValue(lineResultInfo.Brand);
+                            newRow.CreateCell(colIndex++).SetCellValue(paiLunHaoCell);
+                            newRow.CreateCell(colIndex++).SetCellValue(isWheel);//lineResultInfo.IsWheel);
+                            newRow.CreateCell(colIndex++).SetCellValue(lineResultInfo.Length.GetValueOrDefault());
+                            newRow.CreateCell(colIndex++).SetCellValue(lineResultInfo.LeaveLength.GetValueOrDefault());
+                        }
+                    }
+                }
+
+                sheet.AddMergedRegion(new CellRangeAddress(firstRowIndex, firstRowIndex + totalRowCount - 1, 0, 0));
+                rowIndex = firstRowIndex + totalRowCount;
+            }
+
+            var mapper = new Mapper(wkbook);
+
+            mapper.Save(filePath);
+
+
+
+        }
+
+
+    }
+}
---
+++ LineManager-ok/LineManager/LineResultInfoView.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NPOI.HSSF.Record;
+
+namespace LineManager
+{
+
+
+    public class LineResultInfoView
+    {
+
+        public string 出库目的地 { get; set; }
+
+        public string 电缆型号 { get; set; }
+
+
+        public string 生产厂家 { get; set; }
+
+
+        public string 线轮位置 { get; set; }
+
+        public string 是否整轮 { get; set; }
+
+
+        public int 出库长度 { get; set; }
+
+
+        public int 剩余长度 { get; set; }
+
+
+
+        public static List<LineResultInfoView> MapLineResultInfoView(List<LineResultInfo> list)
+        {
+            var result = new List<LineResultInfoView>();
+
+            if (list != null && list.Any())
+            {
+                list.ForEach(m =>
+                {
+
+                    var flag = m.PaiHao.GetValueOrDefault() == 0 || m.LunHao.GetValueOrDefault() == 0;
+
+                    var isWheel = m.LeaveLength.HasValue && m.LeaveLength == 0 ? "是" : m.IsWheel;
+
+                    var lineResult = new LineResultInfoView()
+                    {
+                        出库目的地 = m.CustomerId,
+                        电缆型号 = m.XingHao,
+                        生产厂家 = m.Brand,
+                        线轮位置 = flag ? "库存不足" : $"{m.PaiHao}排 {m.LunHao}号",
+                        是否整轮 = isWheel,//m.IsWheel,
+                        出库长度 = m.Length.GetValueOrDefault(),
+                        剩余长度 = m.LeaveLength.GetValueOrDefault() //m.Length.GetValueOrDefault()
+                    };
+                     
+                    result.Add(lineResult);
+                });
+            }
+
+            return result;
+        }
+
+    }
+}
